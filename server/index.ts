8ed4c7ecb9d18a4becb661ff88b83d629ec66cda
WBelucky <shintaro.kusabiraki@gmail.com>
2020-05-07 02:14:05 +0900

    add server

---
+++ server/index.ts
@@ -0,0 +1,27 @@
+// 参考 https://qiita.com/tetsutaroendo/items/31300a5e475688a2f4ec#%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%A0%E3%82%B5%E3%83%BC%E3%83%90%E3%82%92%E4%BD%9C%E3%82%8B
+// 参考 https://qiita.com/ukyoda/items/a043f999132b05b79525
+import express from "express";
+import next from "next";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, (err?: any) => {
+      if (err) throw err;
+      console.log(`> Ready on https://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
