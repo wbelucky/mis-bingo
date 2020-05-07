adc2b276181e8fb2e34940e8a9aa1ce4257a8f04
WBelucky <shintaro.kusabiraki@gmail.com>
2020-05-07 23:22:23 +0900

    use db in getInitialProps

---
+++ schema/users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    uid SERIAL PRIMARY KEY,
+    name VARCHAR(128) NOT NULL UNIQUE,
+    keyward VARCHAR(128) NOT NULL,
+    twitter_id VARCHAR(128) NOT NULL,
+    generation INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    slack_id VARCHAR(128),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
